cd78d536db35a7d9251c3a8b2d445b07fe16a5c7
ct4842654-prog <ct4842654@gmail.com>
2026-02-17 19:44:33 +0100

    Add files via upload

---
+++ sv.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+with st.sidebar:
+    st.write("Cheikh A T Toure")
+    st.write("Adresse: ouagou niaye2 villa 332")
+    st.write("Tel: 775705642")
+    st.write("Email:cheikhtoure07@gmail.com")
+
+st.header("Profile")
+st.write("""Technicien supérieur en géomatique et topographe avec une solide expérience de terrain 
+    et de traitement de données spatiales. Esprit d’équipe, rigoureux et capable de relever 
+    des défis techniques.""")
+
+st.header("experience professionnel")
+st.write("""
+- Stagiaire Topographe Bureau Topographique DCI
+- Utilisation du GPS, LiDAR et Scanner 3D (SLAM)
+- Traitement des données avec COVADIS, CGEO, etc
+- Logiciel utiliser 
+   COVADIS,
+    CGEO,
+- Logiciels de traitement LiDAR,
+    SIG (QGIS / ArcGIS)
+    """)
+
+st.header("QUALITE")
+st.write("""
+- Esprit d’équipe,
+- Rigueur,
+- Capacité d’adaptation,
+- "Motivation professionnelle""")
+
+
+st.header("Mes compétences")
+st.write("""
+   - Topographie et levés de terrain,
+   - Géomatique et analyse spatiale,
+   - Utilisation du GPS,
+   - Traitement de données LiDAR,
+   - Scanner 3D (SLAM),
+   - Traitement de données topographiques,
+   - Travail en équipe
+   """)
+
+st.header("Formation")
+st.write("""
+- Etudes en géomatique(en cour au CEDT le G15),
+- baccalauréat en 2024 a lycée tivaoune peulh""")
